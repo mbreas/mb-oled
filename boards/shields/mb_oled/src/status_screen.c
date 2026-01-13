1913833ac3ced8e268ea90a9acb1f1bf83f6be67
Marcel Breas <marcel.breas@iodigital.com>
2026-01-13 22:05:33 +0100

    style

--- boards/shields/mb_oled/src/status_screen.c
+++ boards/shields/mb_oled/src/status_screen.c
@@ -10,12 +10,22 @@
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static struct zmk_widget_output_status output_status_widget;
+lv_style_t global_style;
 
 lv_obj_t *zmk_display_status_screen()
 {
   lv_obj_t *screen;
 
   screen = lv_obj_create(NULL);
+
+  lv_style_init(&global_style);
+  lv_style_set_bg_color(&global_style, lv_color_white());
+  lv_style_set_bg_opa(&global_style, LV_OPA_COVER);
+  lv_style_set_text_color(&global_style, lv_color_black());
+  lv_style_set_text_font(&global_style, &lv_font_unscii_8);
+  lv_style_set_text_letter_space(&global_style, 1);
+  lv_style_set_text_line_space(&global_style, 1);
+  lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
 
   zmk_widget_output_status_init(&output_status_widget, screen);
   lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);

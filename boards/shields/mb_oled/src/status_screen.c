9160e1b1d75abc295ebad08228c315a6b684f0ea
Marcel Breas <marcel.breas@iodigital.com>
2026-01-14 13:24:24 +0100

    leyer

--- boards/shields/mb_oled/src/status_screen.c
+++ boards/shields/mb_oled/src/status_screen.c
@@ -6,20 +6,31 @@
 #include "status_screen.h"
 #include "images.h"
 #include <zephyr/logging/log.h>
+#include <zmk/events/layer_state_changed.h>
+#include <zmk/event_manager.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
+struct layer_status_state
+{
+  uint8_t index;
+  const char *label;
+};
+
+static lv_obj_t *screen = NULL;
+static lv_obj_t *status_image = NULL;
+static lv_obj_t *battery_label = NULL;
 lv_obj_t *zmk_display_status_screen()
 {
   LOG_WRN("Creating minimal test screen");
 
-  lv_obj_t *screen = lv_obj_create(NULL);
+  screen = lv_obj_create(NULL);
 
   // Create a simple label with default styling
-  // lv_obj_t *test_label = lv_label_create(screen);
-  // lv_label_set_text(test_label, "TEST");
-  // lv_obj_center(test_label);
+  battery_label = lv_label_create(screen);
+  lv_label_set_text(battery_label, "TEST");
+  lv_obj_align(battery_label, LV_ALIGN_TOP_LEFT, 0, 0;
 
-  lv_obj_t *status_image = lv_img_create(screen);
+  status_image = lv_img_create(screen);
   if (status_image != NULL)
   {
     lv_img_set_src(status_image, &zeon); // Use your image variable name
@@ -35,3 +46,36 @@ lv_obj_t *zmk_display_status_screen()
   LOG_WRN("Minimal test screen created");
   return screen;
 }
+
+static void set_battery_label(struct layer_status_state state)
+{
+  if (state.label == NULL)
+  {
+    char text[7] = {};
+
+    sprintf(text, "%i", state.index);
+
+    lv_label_set_text(battery_label, text);
+  }
+  else
+  {
+    char text[13] = {};
+
+    snprintf(text, sizeof(text), "%s", state.label);
+
+    lv_label_set_text(battery_label, text);
+  }
+}
+
+static struct layer_status_state layer_status_get_state(const zmk_event_t *eh)
+{
+  uint8_t index = zmk_keymap_highest_layer_active();
+  return (struct layer_status_state){
+      .index = index,
+      .label = zmk_keymap_layer_name(index)};
+}
+
+ZMK_DISPLAY_WIDGET_LISTENER(widget_layer_status, struct layer_status_state, set_battery_label,
+                            layer_status_get_state)
+
+ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);

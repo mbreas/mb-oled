0a7606ac1822a00b7100bd9d7bb1f46f2efe77c6
Marcel Breas <marcel.breas@iodigital.com>
2026-01-14 15:04:30 +0100

    add battery

--- boards/shields/mb_oled/src/status_screen.c
+++ boards/shields/mb_oled/src/status_screen.c
@@ -8,7 +8,10 @@
 
 #include <zephyr/logging/log.h>
 #include <zmk/display.h>
+#include <zmk/battery.h>
 #include <zmk/events/layer_state_changed.h>
+#include <zmk/events/battery_state_changed.h>
+#include <zmk/events/usb_conn_state_changed.h>
 #include <zmk/event_manager.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
@@ -21,6 +24,7 @@ struct layer_status_state
 static lv_obj_t *screen = NULL;
 static lv_obj_t *status_image = NULL;
 static lv_obj_t *layer_label = NULL;
+static lv_obj_t *battery_label = NULL;
 
 lv_obj_t *zmk_display_status_screen()
 {
@@ -28,7 +32,7 @@ lv_obj_t *zmk_display_status_screen()
 
   screen = lv_obj_create(NULL);
 
-  // Create a simple label with default styling
+  // Layer setup
   layer_label = lv_label_create(screen);
   uint8_t index = zmk_keymap_highest_layer_active();
   const char *label = zmk_keymap_layer_name(index);
@@ -46,6 +50,14 @@ lv_obj_t *zmk_display_status_screen()
   }
   lv_obj_align(layer_label, LV_ALIGN_TOP_LEFT, 0, 0);
 
+  battery_label = lv_label_create(screen);
+  uint8_t charge = zmk_battery_state_of_charge();
+  char chargetext[14] = {};
+  snprintf(chargetext, sizeof(chargetext), "Battery: %d%%", charge);
+  lv_label_set_text(battery_label, chargetext);
+  lv_obj_align(battery_label, LV_ALIGN_TOP_RIGHT, 0, 0);
+
+  // image setup
   status_image = lv_img_create(screen);
   if (status_image != NULL)
   {
@@ -83,6 +95,13 @@ static void set_layer_label(struct layer_status_state state)
   }
 }
 
+static void set_battery_label(struct battery_status_state state)
+{
+  char chargetext[14] = {};
+  snprintf(chargetext, sizeof(chargetext), "Battery: %d%%", state.level);
+  lv_label_set_text(battery_label, chargetext);
+}
+
 static struct layer_status_state layer_status_get_state(const zmk_event_t *eh)
 {
   uint8_t index = zmk_keymap_highest_layer_active();
@@ -91,7 +110,27 @@ static struct layer_status_state layer_status_get_state(const zmk_event_t *eh)
       .label = zmk_keymap_layer_name(index)};
 }
 
+static struct battery_status_state battery_status_get_state(const zmk_event_t *eh)
+{
+  const struct zmk_battery_state_changed *ev = as_zmk_battery_state_changed(eh);
+
+  return (struct battery_status_state){
+      .level = (ev != NULL) ? ev->state_of_charge : zmk_battery_state_of_charge(),
+#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
+      .usb_present = zmk_usb_is_powered(),
+#endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
+  };
+}
+
 ZMK_DISPLAY_WIDGET_LISTENER(widget_layer_status, struct layer_status_state, set_layer_label,
                             layer_status_get_state)
 
 ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
+
+ZMK_DISPLAY_WIDGET_LISTENER(widget_battery_status, struct battery_status_state,
+                            set_battery_label, battery_status_get_state)
+
+ZMK_SUBSCRIPTION(widget_battery_status, zmk_battery_state_changed);
+#if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
+ZMK_SUBSCRIPTION(widget_battery_status, zmk_usb_conn_state_changed);
+#endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
